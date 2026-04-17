63c9bcfc4831d59980139468fb7721402b2d861d
greissemendiv <est.greisse.mendiv@unimilitar.edu.co>
2026-04-17 09:31:50 -0500

    Dibujar líneas con colores por región

--- script.js
+++ script.js
@@ -25,3 +25,40 @@ function clearCanvas() {
 
 clearBtn.addEventListener('click', clearCanvas);
 clearCanvas();
+
+function getRegionCode(x, y) {
+    let code = 0;
+    
+    // Bit 1: izquierda
+    if (x < CLIP_WINDOW.x1) code |= 1;
+    // Bit 2: derecha  
+    else if (x > CLIP_WINDOW.x2) code |= 2;
+    
+    // Bit 4: abajo
+    if (y < CLIP_WINDOW.y1) code |= 4;
+    // Bit 8: arriba
+    else if (y > CLIP_WINDOW.y2) code |= 8;
+    
+    return code;
+}
+
+function drawRegionLabels() {
+    ctx.fillStyle = '#666';
+    ctx.font = '14px Arial';
+    ctx.textAlign = 'center';
+    
+    // Esquinas
+    ctx.fillText('1001', CLIP_WINDOW.x1 - 30, CLIP_WINDOW.y1 + 20);
+    ctx.fillText('1000', canvas.width/2, CLIP_WINDOW.y1 + 20);
+    ctx.fillText('1010', CLIP_WINDOW.x2 + 30, CLIP_WINDOW.y1 + 20);
+    
+    ctx.fillText('0001', CLIP_WINDOW.x1 - 30, canvas.height/2);
+    ctx.fillText('0000', canvas.width/2, canvas.height/2);
+    ctx.fillText('0010', CLIP_WINDOW.x2 + 30, canvas.height/2);
+    
+    ctx.fillText('0101', CLIP_WINDOW.x1 - 30, CLIP_WINDOW.y2 + 20);
+    ctx.fillText('0100', canvas.width/2, CLIP_WINDOW.y2 + 20);
+    ctx.fillText('0110', CLIP_WINDOW.x2 + 30, CLIP_WINDOW.y2 + 20);
+}
+
+drawRegionLabels();
